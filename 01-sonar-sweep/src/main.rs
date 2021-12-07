6772f7d2b7b34fbea0ad0976ba032d24a2ae388b
luke9kim8 <luke9kim8@gmail.com>
2021-12-07 00:25:04 -0500

    add day 1 soln 2

--- 01-sonar-sweep/src/main.rs
+++ 01-sonar-sweep/src/main.rs
@@ -1,16 +1,17 @@
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{self, BufReader, BufRead};
 use std::io::Lines;
 
 fn main() {
     soln1();
+    soln2();
 }
 
-pub fn read_lines(path: String) -> Lines<BufReader<File>> {
+fn read_lines(path: String) -> Lines<BufReader<File>> {
   BufReader::new(File::open(path).unwrap()).lines()
 }
 
-pub fn soln1() {
+fn soln1() {
   let buf_lines = read_lines("./input.txt".to_string());
   let mut prev_val = 0;
   let mut counter = 0;
@@ -26,4 +27,22 @@ pub fn soln1() {
     prev_val = depth;
   }
   println!("counter: {}", counter);
+}
+
+fn soln2() {
+  let depths: Vec<u32> = read_lines(String::from("./input.txt")).map(|x| x.unwrap().parse::<u32>().unwrap()).collect();
+  let mut prev_sum = depths[0] + depths[1] + depths[2];
+  let mut count: u32 = 0;
+  for (i, curr_depth) in depths.iter().map(|x| x.clone()).enumerate() {
+    if i < 3 {
+      continue;
+    } else {
+      let new_sum = prev_sum - depths[i - 3] + curr_depth;
+      if new_sum > prev_sum {
+        count += 1;
+      }
+      prev_sum = new_sum;
+    }
+  }
+  println!("soln: {}", count);
 }
